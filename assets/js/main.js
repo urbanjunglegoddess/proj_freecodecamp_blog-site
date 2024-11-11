66e8f9cba83365158e152c7ba51daec726e7b5b6
Omegea Hunter <omegeadhunter@gmail.com>
2024-11-11 09:13:53 -0500

    Add a utility function to select DOM elements with error handling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,11 @@
 // Grab elements
+const selectElement = selector => {
+    const element = document.querySelector(selector); // select the element element
+    if(element) return element;
+    throw new Error(`Something went wrong! Make sure that ${selector} exists/is typed correctly.`);  // throw an error
 
+}
+console.log(selectElement('.footer'));
 
 //Nav styles on scroll
 
