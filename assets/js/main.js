37e219cc1983b55a6949f4a6e3507d9b394fc4fb
Omegea Hunter <omegeadhunter@gmail.com>
2024-11-11 10:26:05 -0500

    Fix scroll header functionality and update element selectors for menu and search pop-up

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,20 +4,27 @@ const selectElement = (selector) => {
     if(element) return element;
     throw new Error(`Something went wrong! Make sure that ${selector} exists/is typed correctly.`);  
 };
-//Nav styles on scroll
-const scrollHeader=( )=> {
-    constheaderElement = selectElement('#header');
-    if(this.scrollY >=15){
-        headerElement.classlist.add('activated');}
-    else{
-        headerElement.classlist.remove('activated');}
-}; 
-window.addEventListener('scroll', scrollHeader);
-// Open menu & search pop-up
-const menuToggleIcon = selectElement('.menu-toggle-icon');
 
-const toggleMenu = ( )=> {
-    const mobileMenu =selectElement('#menu');
+//Nav styles on scroll
+const scrollHeader = () =>{
+    const navbarElement = selectElement('#header');
+    if(this.scrollY >= 15) {
+        navbarElement.classList.add('activated');
+    } else {
+        navbarElement.classList.remove('activated');
+    }
+}
+
+window.addEventListener('scroll', scrollHeader);
+
+// Open menu & search pop-up
+const menuToggleIcon = selectElement('#menu-toggle-icon');
+const formOpenBtn = selectElement('#search-icon');
+const formCloseBtn = selectElement('#form-close-btn');
+const searchContainer = selectElement('#search-form-container');
+
+const toggleMenu = () =>{
+    const mobileMenu = selectElement('#menu');
     mobileMenu.classList.toggle('activated');
     menuToggleIcon.classList.toggle('activated');
 }
@@ -53,5 +60,4 @@ themeToggleBtn.addEventListener('click', function () {
         localStorage.removeItem('currentTheme');
     }
 });
-
 // Swiper
