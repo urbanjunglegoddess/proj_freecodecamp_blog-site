ea9aae5f08e43bca050030d5dfd8f92ec8efe24b
Omegea Hunter <omegeadhunter@gmail.com>
2024-11-11 12:25:20 -0500

    "Updated CSS and JS files to add new styles and functionality for the Quick Read section, and added HTML for the Quick Read section in index.html."

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,3 +61,15 @@ themeToggleBtn.addEventListener('click', function () {
     }
 });
 // Swiper
+const swiper = new Swiper('.swiper', {
+slidesPerView: 1,
+spaceBetween: 20,
+navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+},
+pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+},
+})
