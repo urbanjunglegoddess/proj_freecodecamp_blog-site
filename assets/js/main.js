70278937909081708198d086ce477900bd6d7e76
Omegea Hunter <omegeadhunter@gmail.com>
2024-11-11 09:35:19 -0500

    "Updated selectElement function and added scrollHeader function"

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,18 @@
 // Grab elements
-const selectElement = selector => {
-    const element = document.querySelector(selector); // select the element element
+const selectElement = (selector) => {
+    const element = document.querySelector(selector);
     if(element) return element;
-    throw new Error(`Something went wrong! Make sure that ${selector} exists/is typed correctly.`);  // throw an error
-
-}
-console.log(selectElement('.footer'));
-
+    throw new Error(`Something went wrong! Make sure that ${selector} exists/is typed correctly.`);  
+};
 //Nav styles on scroll
+const scrollHeader=( )=> {
+    constheaderElement = selectElement('#header');
+    if(this.scrollY >=15){
+        headerElement.classlist.add('activated');}
+    else{
+        headerElement.classlist.remove('activated');}
+}; 
+
 
 // Open menu & search pop-up
 
