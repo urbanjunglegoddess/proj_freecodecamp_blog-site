15d38330e2a5d11e4504790880792a4a969e0d2d
Omegea Hunter <omegeadhunter@gmail.com>
2024-11-11 09:49:08 -0500

    "Added event listeners for scroll, menu toggle, and theme toggle; updated JavaScript code for theme switching and local storage; added search div to HTML"

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,14 +12,42 @@ const scrollHeader=( )=> {
     else{
         headerElement.classlist.remove('activated');}
 }; 
-
-
+window.addEventListener('scroll', scrollHeader);
 // Open menu & search pop-up
+const menuToggleIcon = selectElement('.menu-toggle-icon');
+
+const toggleMenu = ( )=> {
+    const mobileMenu =selectElement('#menu');
+    mobileMenu.classList.toggle('activated');
+    menuToggleIcon.classList.toggle('activated');
+}
+
+menuToggleIcon.addEventListener('click', toggleMenu);
 
 // Open/Close search form popup
 
 // -- Close the search form popup on ESC keypress
 
 // Switch theme/add to local storage
+const body = document.body;
+const themeToggleBtn = selectElement('#theme-toggle-btn');
+const currentTheme = localStorage.getItem('currentTheme');
+
+// Check to see if there is a theme preference in local Storage, if so add the ligt theme to the body
+if (currentTheme) {
+    body.classList.add('light-theme');
+}
+
+themeToggleBtn.addEventListener('click', function () {
+    // Add light theme on click
+    body.classList.toggle('light-theme');
+
+    // If the body has the class of light theme then add it to local Storage, if not remove it
+    if (body.classList.contains('light-theme')) {
+        localStorage.setItem('currentTheme', 'themeActive');
+    } else {
+        localStorage.removeItem('currentTheme');
+    }
+});
 
 // Swiper
